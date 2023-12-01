390d41cd03dc784e22c38a6024f727af3c44c8f0
Arcii <47848907+Arcii@users.noreply.github.com>
2023-12-01 15:45:43 +0100

    -Created and designed OperatorHomeGUI

--- src/main/java/org/climatemonitoring/client/gui/views/OperatorHomeGUI.java
+++ src/main/java/org/climatemonitoring/client/gui/views/OperatorHomeGUI.java
@@ -22,6 +22,48 @@ public class OperatorHomeGUI extends JFrame{
     private JLabel stateLabel;
     private JTextField stateField;
     private JButton cercaPerDenominazioneButton;
+    private JPanel searchPanel;
+    private JTextField windField;
+    private JTextField humidityField;
+    private JTextField pressureField;
+    private JPanel surveysPanel;
+    private JPanel surveyFormPanel;
+    private JTextField temperatureField;
+    private JTextField precipitationField;
+    private JTextField glacial_altitudeField;
+    private JTextField glacial_massField;
+    private JButton logoutButton;
+    private JLabel windLabel;
+    private JLabel humidityLabel;
+    private JLabel pressureLabel;
+    private JLabel temperatureLabel;
+    private JLabel precipitationLabel;
+    private JLabel glacial_altitudeLabel;
+    private JLabel glacial_massLabel;
+    private JLabel surveyInfoLabel;
+    private JLabel climateCategoryLabel;
+    private JLabel explanationLabel;
+    private JLabel scoreLabel;
+    private JLabel notesLabel;
+    private JTextField windNoteField;
+    private JTextField humidityNoteField;
+    private JTextField pressureNoteField;
+    private JTextField temperatureNoteField;
+    private JTextField precipitationNoteField;
+    private JTextField glacial_altitudeNoteField;
+    private JTextField glacial_massNoteField;
+    private JLabel windExplanationLabel;
+    private JLabel humidityExplanationLabel;
+    private JLabel pressureExplanationLabel;
+    private JLabel temperatureExplanationLabel;
+    private JLabel precipitationExplanationLabel;
+    private JLabel glacial_altitudeExplanationLabel;
+    private JLabel glacial_massExplanationLabel;
+    private JPanel poiSelectionPanel;
+    private JTable poiSelectionTable;
+    private JScrollPane poiSelectionScroll;
+    private JButton addSurveyButton;
+    private JButton addPoiToCenterButton;
 
     public OperatorHomeGUI(){
         setTitle("Climate Monitoring");
@@ -31,142 +73,6 @@ public class OperatorHomeGUI extends JFrame{
         coordinatesSearchPanel.setSize(400,400);
         nameSearchPanel.setSize(400,400);
         new OperatorHomeGUIController(this);
-    }
-
-    public JPanel getOperatorHomePanel() {
-        return operatorHomePanel;
-    }
-
-    public void setOperatorHomePanel(JPanel operatorHomePanel) {
-        this.operatorHomePanel = operatorHomePanel;
-    }
-
-    public JLabel getClimatemonitoringLabel() {
-        return climatemonitoringLabel;
-    }
-
-    public void setClimatemonitoringLabel(JLabel climatemonitoringLabel) {
-        this.climatemonitoringLabel = climatemonitoringLabel;
-    }
-
-    public JLabel getGreetingLabel() {
-        return greetingLabel;
-    }
-
-    public void setGreetingLabel(JLabel greetingLabel) {
-        this.greetingLabel = greetingLabel;
-    }
-
-    public JPanel getCoordinatesSearchPanel() {
-        return coordinatesSearchPanel;
-    }
-
-    public void setCoordinatesSearchPanel(JPanel coordinatesSearchPanel) {
-        this.coordinatesSearchPanel = coordinatesSearchPanel;
-    }
-
-    public JLabel getCoordinatesSearchLabel() {
-        return coordinatesSearchLabel;
-    }
-
-    public void setCoordinatesSearchLabel(JLabel coordinatesSearchLabel) {
-        this.coordinatesSearchLabel = coordinatesSearchLabel;
-    }
-
-    public JLabel getLatitudeLabel() {
-        return latitudeLabel;
-    }
-
-    public void setLatitudeLabel(JLabel latitudeLabel) {
-        this.latitudeLabel = latitudeLabel;
-    }
-
-    public JTextField getLatitudeField() {
-        return latitudeField;
-    }
-
-    public void setLatitudeField(JTextField latitudeField) {
-        this.latitudeField = latitudeField;
-    }
-
-    public JLabel getLongitudeLabel() {
-        return longitudeLabel;
-    }
-
-    public void setLongitudeLabel(JLabel longitudeLabel) {
-        this.longitudeLabel = longitudeLabel;
-    }
-
-    public JTextField getLongitudeField() {
-        return longitudeField;
-    }
-
-    public void setLongitudeField(JTextField longitudeField) {
-        this.longitudeField = longitudeField;
-    }
-
-    public JButton getCoordinatesSearchButton() {
-        return coordinatesSearchButton;
-    }
-
-    public void setCoordinatesSearchButton(JButton coordinatesSearchButton) {
-        this.coordinatesSearchButton = coordinatesSearchButton;
-    }
-
-    public JPanel getNameSearchPanel() {
-        return nameSearchPanel;
-    }
-
-    public void setNameSearchPanel(JPanel nameSearchPanel) {
-        this.nameSearchPanel = nameSearchPanel;
-    }
-
-    public JLabel getNameSearchLabel() {
-        return nameSearchLabel;
-    }
-
-    public void setNameSearchLabel(JLabel nameSearchLabel) {
-        this.nameSearchLabel = nameSearchLabel;
-    }
-
-    public JLabel getNameLabel() {
-        return nameLabel;
-    }
-
-    public void setNameLabel(JLabel nameLabel) {
-        this.nameLabel = nameLabel;
-    }
-
-    public JTextField getNameField() {
-        return nameField;
-    }
-
-    public void setNameField(JTextField nameField) {
-        this.nameField = nameField;
-    }
-
-    public JLabel getStateLabel() {
-        return stateLabel;
-    }
-
-    public void setStateLabel(JLabel stateLabel) {
-        this.stateLabel = stateLabel;
-    }
-
-    public JTextField getStateField() {
-        return stateField;
-    }
-
-    public void setStateField(JTextField stateField) {
-        this.stateField = stateField;
-    }
-
-    public JButton getCercaPerDenominazioneButton() {
-        return cercaPerDenominazioneButton;
-    }
-
-    public void setCercaPerDenominazioneButton(JButton cercaPerDenominazioneButton) {
-        this.cercaPerDenominazioneButton = cercaPerDenominazioneButton;
     }
 
 }
